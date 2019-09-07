681eb33ce16ddb88af53e2b0f04110b3ff52bb96
SL Lee <wayuer19@sjtu.edu.cn>
2019-09-07 19:40:48 +0800

    Update gro2gro.py

--- gro2gro.py
+++ gro2gro.py
@@ -5,13 +5,13 @@ import sys,os,subprocess
 import numpy as np
 import string
 
-f1 = open("md.gro","r")
-n = 3
-m = 5
-dp = int(raw_input("the number of dp: "))
-nab = (n+m)*dp
+f1 = open("md.gro","r")  ##input the original gro file
+n = 3   ##number of polar bead
+m = 5   ##number of apolar bead
+dp = int(raw_input("the number of dp: "))   ##input the number of DP from keyboard
+nab = (n+m)*dp   #bead number of one polymer chain
 
-f2 = open('out.gro','w')
+f2 = open('out.gro','w')  ##output file
 
 lines = [] 
 for line in f1.readlines():
@@ -22,8 +22,8 @@ nmol = float(line[1])/(nab)
 for i in range(nmol):
     idx1 = i*nab + 2
     idx2 = idx1 + (n+m)*(dp-1) + 2
-    lines[idx1] = lines[idx1].replace('O','P')
-    lines[idx2] = lines[idx2].replace('N','B')
+    lines[idx1] = lines[idx1].replace('O','P')  #replace "O" as "p"
+    lines[idx2] = lines[idx2].replace('N','B')  #replace "N" as "B"
 
 for line in lines:
     print >> f2, line
